5dad4b3e219d16143a65527b4fdf3f8a699fb129
Rawier <alexam211@hotmail.com>
2025-08-08 21:17:23 -0600

    fix: color bug

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -332,7 +332,7 @@ const Portfolio: NextPage<PortfolioProps> = ({ translations }) => {
                         data-ripple-light="true"
                         type="button"
                         onClick={() => window.open(item.GoToLink, "_blank")}
-                        className="relative px-4 py-2 overflow-hidden font-medium duration-300 border border-b-4 rounded-md outline-none text-lime-400 bg-lime-900 border-lime-400 hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 group"
+                        className="relative px-4 py-2 overflow-hidden font-medium duration-300 border border-b-4 rounded-md outline-none text-green-400 bg-green-900 border-green-400 hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 group"
                       >
                         <span className="bg-violet-400 shadow-violet-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
                         Go to
